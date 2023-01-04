335f6c9f8512996d0c20a1e0303e03b5f49d62cf
denchu314 <42888401+denchu314@users.noreply.github.com>
2023-01-04 22:45:39 +0900

    bug fix

--- main.cpp
+++ main.cpp
@@ -7,13 +7,22 @@
 using namespace std;
 using namespace Eigen;
 
-void calcKalmanFilter(double & xhat_new, double & P_new, double & g, double A, double b, double bu, double c, double rho_vv, double rho_ww, double u, double y, double xhat,double P)
+// 雑音信号の生成
+const int	N 	= 1000;	//データ数
+typedef double matrixType;
+typedef Matrix<matrixType, 2, N> Matrix_2xN_f;
+typedef Matrix<matrixType, 1, N> Matrix_1xN_f;
+typedef Matrix<matrixType, 2, 2> Matrix_2x2_f;
+typedef Matrix<matrixType, 2, 1> Matrix_2x1_f;
+
+void calcKalmanFilter(Matrix_2xN_f & xhat_new, Matrix_2x2_f & P_new, Matrix_2x1_f & g, Matrix_2x2_f A, Matrix_2x1_f b, Matrix_2x1_f bu, Matrix_2x1_f c, double rho_vv, double rho_ww, double u, Matrix_1xN_f y, Matrix_2xN_f xhat, Matrix_2x2_f P, std::size_t k)
 {
-	double	xhatm 		= A*xhat + bu * u;
-	double	Pm 		= A * P * A.transpose() + rho_vv * b * b.transpose();
-	g 			= (Pm * c)/(c.transpose() * P * c + rho_ww);
-	xhat_new		= xhatm + g * (y - c.transpose() * xhatm);
-	P_new			= (1 - g * c.transpose()) * Pm;
+	Matrix_2x2_f	I = Matrix_2x2_f::Identity(2,2);
+	Matrix_2x1_f	xhatm 		= A*xhat.col(k) + bu * u;
+	Matrix_2x2_f	Pm 		= A * P * A.transpose() + rho_vv * b * b.transpose();
+	g 				= (Pm * c)/(c.transpose() * P * c + rho_ww);
+	xhat_new.col(k+1)		= xhatm + g * (y(k) - c.transpose() * xhatm);
+	P_new				= (I - g * c.transpose()) * Pm;
 	return;
 }
 
@@ -23,21 +32,14 @@ int main()
 	double		rho_vv 	= 0.01;	//システム雑音の分散
 	double		u 	= 0.0;	//制御の値
 	double		rho_ww 	= 10.0;	//観測雑音の分散
-	const int	N 	= 1000;	//データ数
 	double		delta_t = 0.05;	
 
 	double		k_spring	= 1.0;	//ばね定数
 	double		m_mass		= 1.0;	//質量
-	std::ofstream file("normal_distribution.tsv");
+	std::ofstream file("normal_distribution.csv");
 
-	// 観測データ生成
-	// 雑音信号の生成
-	typedef double matrixType;
-	typedef Matrix<matrixType, 2, N> Matrix_2xN_f;
-	typedef Matrix<matrixType, 1, N> Matrix_1xN_f;
-	typedef Matrix<matrixType, 2, 2> Matrix_2x2_f;
-	typedef Matrix<matrixType, 2, 1> Matrix_2x1_f;
 	
+	// 観測データ生成
 	Matrix_2x2_f	A;					//プラントシステム
 	Matrix_2x2_f	P;	//共分散行列
 	Matrix_2x1_f	b;	//システム雑音の行列
@@ -92,16 +94,14 @@ int main()
 		y(k) = c.transpose() * x.col(k) + w(k);
 	}
 	
-
+	//cout << xhat.col(1) << endl;
 	for (std::size_t k = 0; k < (N - 1); ++k) {
-		calcKalmanFilter(xhat.col(k+1), P, g, A, b, bu, c, rho_vv, rho_ww, u, y(k), xhat.col(k), P);
+		calcKalmanFilter(xhat, P, g, A, b, bu, c, rho_vv, rho_ww, u, y, xhat, P, k);
 	}
 	
-	file << "time" << "," << "true x1" << "," << "true x2" << "," << "estimated xhat" << "," << "measured y" << "\t\n";
+	file << "time" << "," << "true x1" << "," << "true x2" << "," << "estimated xhat1" << "," << "estimated xhat2" << ","  << "measured y" << "\t\n";
 	for (std::size_t k = 0; k < (N - 1); ++k) {
-		//cout << x.col(k).row(0) << x.col(k).row(1) << endl;
-		file << (double)k*delta_t << "," << x.col(k).row(0) << "," << x.col(k).row(1) << "," << "," << y(k) << endl;
-		//file << x.col(k) << "\t\n";
+		file << (double)k*delta_t << "," << x.col(k).row(0) << "," << x.col(k).row(1) << "," << xhat.col(k).row(0) << "," << xhat.col(k).row(1) << "," << y(k) << endl;
 	}
 //	for (std::size_t k = 0; k < (N - 1); ++k) {
 //		//file << x.col(k+1) << "," << xhat.col(k+1) << "," << y(k+1) << "\t\n";
